5481de46e855bee02711db24813d70b93a18ffb7
EmanuelCode19 <Emanuel_4554@outlook.com>
2026-08-03 19:32:12 -0400

    test

--- src/test/java/hooks/Hook.java
+++ src/test/java/hooks/Hook.java
@@ -6,25 +6,24 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 public class Hook {
     public static WebDriver driver;
-    FirefoxOptions options = new FirefoxOptions();
+    ChromeOptions options = new ChromeOptions();
 
 
     @Before
     public void setUp(){
         System.out.println("Configurando el entorno de pruebas");
-        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-            options.addArguments("--headless");
-            options.addArguments("--width=1920");
-            options.addArguments("--height=1080");
-        }
-        driver = new FirefoxDriver();
-    }
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);    }
 
     @After
     public void tearDown(Scenario scenario) {
