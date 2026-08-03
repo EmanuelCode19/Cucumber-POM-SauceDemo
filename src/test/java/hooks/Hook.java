aca358ce2c6eca0bc38cf28c41e4a16362b7ce47
EmanuelCode19 <Emanuel_4554@outlook.com>
2026-08-03 19:24:55 -0400

    adding actions

--- src/test/java/hooks/Hook.java
+++ src/test/java/hooks/Hook.java
@@ -7,25 +7,38 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 public class Hook {
     public static WebDriver driver;
+    FirefoxOptions options = new FirefoxOptions();
 
 
     @Before
     public void setUp(){
         System.out.println("Configurando el entorno de pruebas");
+        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+            options.addArguments("--headless");
+            options.addArguments("--width=1920");
+            options.addArguments("--height=1080");
+        }
         driver = new FirefoxDriver();
-        driver.manage().window().maximize();
     }
 
     @After
-    public void tearDown(Scenario scenario){
+    public void tearDown(Scenario scenario) {
         System.out.println("Terminando los procesos");
-        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        scenario.attach(screenshot, "image/png", "Screenshot en falla");
-        if(driver != null){
+
+        if (driver != null) {
+
+            if (scenario.isFailed()) {
+                byte[] screenshot = ((TakesScreenshot) driver)
+                        .getScreenshotAs(OutputType.BYTES);
+
+                scenario.attach(screenshot, "image/png", "Screenshot");
+            }
+
             driver.quit();
         }
     }
